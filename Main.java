6ea1a40ac41ad63f3ec6b528b335f78a25c3c93c
chan-98 <84775285+chan-98@users.noreply.github.com>
2021-05-30 09:17:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int a = 2;
+    int b = 10;
+    int c = a + b;
+      System.out.println (c);
+  }
+}
